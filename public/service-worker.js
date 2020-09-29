6414f79e8eba560709acc72bf66f279534ba9590
Wilson Birch <wilsonbirch@gmail.com>
2020-09-29 01:05:39 -0400

    updated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,54 +8,72 @@ const FILES_TO_CACHE = [
   './styles.css'
 ];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const CACHE_NAME = 'budget-cache';
+const DATA_CACHE_NAME = 'data-budget-cache';
 
+//install
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Your files were pre-cached successfully");
+        return cache.addAll(FILES_TO_CACHE);
+      }) 
       .then(self.skipWaiting())
   );
 });
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
+  const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
   event.waitUntil(
     caches
       .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-      })
-      .then((cachesToDelete) => {
+      .then((keyList) => {
         return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            };
           })
-        );
+        )
       })
       .then(() => self.clients.claim())
   );
 });
 
-// self.addEventListener('fetch', (event) => {
-//   if (event.request.url.startsWith(self.location.origin)) {
-//     event.respondWith(
-//       caches.match(event.request).then((cachedResponse) => {
-//         if (cachedResponse) {
-//           return cachedResponse;
-//         }
+//fetch
+self.addEventListener('fetch', (event) => {
+  //cache successful requests to API
+  if (event.request.url.includes("/api")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME)
+      .then(cache => {
+        return fetch(event.request);
+      })
+      .then(response => {
+        //if response is good, clone and store in cache
+        if (response.status === 200) {
+          cache.put(event.request.url, response.clone());
+        };
 
-//         return caches.open(RUNTIME).then((cache) => {
-//           return fetch(event.request).then((response) => {
-//             return cache.put(event.request, response.clone()).then(() => {
-//               return response;
-//             });
-//           });
-//         });
-//       })
-//     );
-//   }
-// });
+        return response;
+
+      })
+      .catch(err => {
+        // Network request failed try to get it from cache
+        return cache.match(event.request);
+      })
+      .catch(err => console.log(err))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);   
+    })
+  );
+});
