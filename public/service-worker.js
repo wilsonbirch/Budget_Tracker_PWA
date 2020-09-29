e41fff451f1b8df3d0198bd57b4b308a6b20168f
Wilson Birch <wilsonbirch@gmail.com>
2020-09-29 00:48:40 -0400

    updated service-worker script for index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -40,22 +40,22 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.url.startsWith(self.location.origin)) {
+//     event.respondWith(
+//       caches.match(event.request).then((cachedResponse) => {
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         }
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-});
+//         return caches.open(RUNTIME).then((cache) => {
+//           return fetch(event.request).then((response) => {
+//             return cache.put(event.request, response.clone()).then(() => {
+//               return response;
+//             });
+//           });
+//         });
+//       })
+//     );
+//   }
+// });
