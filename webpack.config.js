9396a88dd20ddbd4e6b80f65135a84ff767e552f
Wilson Birch <wilsonbirch@gmail.com>
2020-09-29 20:59:17 -0400

    updated webpack to create manifest in /dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,43 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
 const config = {
     entry: "./public/index.js",
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
     },
-    mode: "development"
-}
+    mode: "development",
+    plugins: [
+        new WebpackPwaManifest({
+
+            filename: "manifest.json",
+
+            inject: false,
+
+            fingerprints: false,
+
+            name: "Budget App",
+            short_name: "Budget",
+            theme_color: "#ffffff",
+            background_color: "#ffffff",
+            start_url: "/",
+            display: "standalone",
+
+            icons: [
+                {
+                    "src": "public/icons/icon-192x192.png",
+                    "sizes": "192x192",
+                    "type": "image/png"
+                  },
+                  {
+                    "src": "public/icons/icon-512x512.png",
+                    "sizes": "512x512",
+                    "type": "image/png"
+                  }
+            ]
+        })
+    ]
+};
 
 module.exports = config;
